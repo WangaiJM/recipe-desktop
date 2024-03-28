2b5bbfa1af212789be31a1d3b72dc5c747d21ec7
WangaiJM <jahkeyjohn@gmail.com>
2024-03-29 01:50:27 +0300

    css

--- newshomepage/vite.config.js
+++ newshomepage/vite.config.js
@@ -7,4 +7,13 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/assets/css/_variables.scss";
+        `,
+      },
+    },
+  },
 });
