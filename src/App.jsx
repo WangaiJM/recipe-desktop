4c9ade74f62e7e9743891d30b3fd7a2fb09e5587
WangaiJM <jahkeyjohn@gmail.com>
2024-03-29 01:50:11 +0300

    preprocessed css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import "./App.css";
+import "./App.scss";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App;
