36b6fc6ac6edb2c963f2d7b65d79585fab26dad1
WangaiJM <jahkeyjohn@gmail.com>
2024-03-29 04:58:16 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,53 @@
 import "./header.scss";
+import Logo from "./../../assets/images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 
 const Header = () => {
-  return <header>Header</header>;
+  const [active, setActive] = useState(false);
+
+  const handleMenu = () => {
+    setActive(!active);
+  };
+  return (
+    <header>
+      <div className="container">
+        <a href="/">
+          <img src={Logo} alt="Main logo" />
+        </a>
+        <nav className={`main-nav ${active ? "active" : ""}`}>
+          <div className="nav-items">
+            <a href="" className="nav__link">
+              Home
+            </a>
+            <a href="" className="nav__link">
+              New
+            </a>
+            <a href="" className="nav__link">
+              Popular
+            </a>
+            <a href="" className="nav__link">
+              Trending
+            </a>
+            <a href="" className="nav__link">
+              Categories
+            </a>
+          </div>
+        </nav>
+        <div className="hambuger">
+          <FaBars
+            className={`hambuger-btn ${active ? "" : "active"}`}
+            onClick={handleMenu}
+          />
+          <IoClose
+            className={`hambuger-btn ${active ? "active" : ""}`}
+            onClick={handleMenu}
+          />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
